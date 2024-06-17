6771066d454f02323eea90f8b8a745dbad81583b
jaspreet kaur <jaspreetkaur17@academic.rrc.ca>
2024-06-17 13:58:37 -0500

    Added show action and view for Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/products_controller.rb
 class ProductsController < ApplicationController
-  def index
-    @products = Product.all
+  def show
+    @product = Product.find(params[:id])
   end
 end
